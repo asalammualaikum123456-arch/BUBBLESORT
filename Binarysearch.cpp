dfb11c1d248e16f4f15aecbecad12ac228d95404
asalammualaikum123456-arch <asalammualaikum123456@gmail.com>
2026-04-16 15:59:03 +0700

    menambahkan display

--- Binarysearch.cpp
+++ Binarysearch.cpp
@@ -53,3 +53,10 @@ void bubbleSortArray()
     } while (pass < nPanjang - 1);
 }
 
+void display()
+{
+    cout << "\n======================================\n";
+    cout << "   Elemen Array Setelah Diurutkan (Asc)\n";
+    cout << "======================================\n";
+
+    
