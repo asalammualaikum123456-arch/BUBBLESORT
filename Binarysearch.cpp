979841a3973bc6dd1282ac323c14563a100b540f
asalammualaikum123456-arch <asalammualaikum123456@gmail.com>
2026-04-16 16:03:45 +0700

    menambahkan display header untuk pencarian binarysearch

--- Binarysearch.cpp
+++ Binarysearch.cpp
@@ -70,3 +70,16 @@ void display()
     cout << endl;
 }
 
+void binarySearch()
+{
+    char ulang;
+    do
+    {
+        cout << "\n=====================================\n";
+        cout << "    Pencarian Binary Search             \n";
+        cout << "=====================================\n";
+
+        cout << "Masukkan elemen yang ingin dicari: ";
+        cin >> x;
+        
+        
