42d929b040f1c6dfb240ce001d19af3c07807c39
asalammualaikum123456-arch <asalammualaikum123456@gmail.com>
2026-04-16 15:52:30 +0700

    membuat fungsi input dan menambahkan validasi looping

--- Binarysearch.cpp
+++ Binarysearch.cpp
@@ -5,3 +5,20 @@ int element[10];
 int nPanjang;
 int x;
 
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan banyaknya arrayn(maksimal 10): ";
+        cin >> nPanjang;
+
+        if (nPanjang <= 10)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\n[!] jumlah elemen tidak boleh lebih dari 10. Silahkan coba lagi.\n";
+        }
+       
+    }
